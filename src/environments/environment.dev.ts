64c998aa0cffb4826bf48fe7327165997254a593
Stéphane Bouget <stephane.bouget@orange.com>
2023-05-24 10:29:10 +0200

    Auto load file in local to debug

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,7 @@ import { covisualizationCommonEnvironment } from './covisualization-common'
 import { visualizationCommonEnvironment } from './visualization-common'
 
 export const AppConfig = {
+	debugFile: false,
 	production: false,
 	environment: 'DEV',
 	visualizationCommon: visualizationCommonEnvironment,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ import { covisualizationCommonEnvironment } from './covisualization-common'
 import { visualizationCommonEnvironment } from './visualization-common'
 
 export const AppConfig = {
+	debugFile: false,
 	production: true,
 	environment: 'PROD',
 	visualizationCommon: visualizationCommonEnvironment,
