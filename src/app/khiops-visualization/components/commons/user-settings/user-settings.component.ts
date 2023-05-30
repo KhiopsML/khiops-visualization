fa9661f52195199918724819e60e784be6b198b4
gregory collot <gregory.collot.ext@orange.com>
2023-05-30 11:16:24 +0200

    Update app.component.ts, user-settings.component.ts, app.component.ts, and 2 more files

--- src/app/khiops-visualization/components/commons/user-settings/user-settings.component.ts
+++ src/app/khiops-visualization/components/commons/user-settings/user-settings.component.ts
@@ -20,14 +20,6 @@ import {
 	TranslateService
 } from '@ngstack/translate';
 
-// TODO remove electron
-// let storage;
-// try {
-// 	storage = require('electron-json-storage');
-// } catch (e) {
-// 	console.warn('Can not access storage', e);
-// }
-
 @Component({
 	selector: 'app-user-settings',
 	templateUrl: './user-settings.component.html',
@@ -40,7 +32,6 @@ export class UserSettingsComponent implements OnChanges {
 	numberPrecisionExample;
 	numberPrecision;
 	liftCurveSmooth: number;
-	// liftCurveSmoothExample: string;
 	fontSize: any;
 	contrastValue: number;
 	allowCookies: boolean;
@@ -61,11 +52,6 @@ export class UserSettingsComponent implements OnChanges {
 
 		this.khiopsLibraryService.trackEvent('page_view', 'settings');
 
-		// Font size
-		// this.fontSize = localStorage.getItem(AppConfig.common.GLOBAL.LS_ID + 'FONT_SIZE') || AppConfig.common.GLOBAL.FONT_SIZE;
-		// localStorage.setItem(AppConfig.common.GLOBAL.LS_ID + 'FONT_SIZE', this.fontSize);
-		// AppConfig.common.GLOBAL.FONT_SIZE = this.fontSize;
-
 		// Global number precision
 		this.numberPrecision = localStorage.getItem(AppConfig.visualizationCommon.GLOBAL.LS_ID + 'SETTING_NUMBER_PRECISION') || AppConfig.visualizationCommon.GLOBAL.TO_FIXED;
 		localStorage.setItem(AppConfig.visualizationCommon.GLOBAL.LS_ID + 'SETTING_NUMBER_PRECISION', this.numberPrecision);
@@ -76,17 +62,8 @@ export class UserSettingsComponent implements OnChanges {
 		localStorage.setItem(AppConfig.visualizationCommon.GLOBAL.LS_ID + 'SETTING_MATRIX_CONTRAST', this.contrastValue.toString());
 		AppConfig.visualizationCommon.GLOBAL.MATRIX_CONTRAST = this.contrastValue;
 
-		// Curve lift smooth datas
-		// this.liftCurveSmooth = parseInt(localStorage.getItem(AppConfig.common.GLOBAL.LS_ID + 'LIFT_CURVE_SMOOTH'), 10) || AppConfig.common.LIFT_CURVE.LIFT_CURVE_SMOOTH;
-		// localStorage.setItem(AppConfig.common.GLOBAL.LS_ID + 'LIFT_CURVE_SMOOTH', this.liftCurveSmooth.toString());
-		// AppConfig.common.LIFT_CURVE.LIFT_CURVE_SMOOTH = this.liftCurveSmooth;
-		// this.liftCurveSmoothExample = this.translate.get('SETTINGS.ONE_DATA_OUT_OF', {
-		// 	count: this.liftCurveSmooth.toString()
-		// });
-
 		// Allow cookies
-		// TODO remove electron
-		// this.allowCookies = (storage.getSync('COOKIE_CONSENT') === 'true') || false;
+		this.allowCookies = (localStorage.getItem('COOKIE_CONSENT_VISU') === 'true') || false;
 		this.initialAllowCookies = _.cloneDeep(this.allowCookies);
 
 		// theme
@@ -96,14 +73,6 @@ export class UserSettingsComponent implements OnChanges {
 	}
 
 	onClickOnCancel() {
-		// reset font_size on cancel
-		// const previousFontSize = localStorage.getItem(AppConfig.common.GLOBAL.LS_ID + 'FONT_SIZE');
-		// this.onFontSizeChanged({
-		// 	value: previousFontSize
-		// });
-		// localStorage.setItem(AppConfig.common.GLOBAL.LS_ID + 'FONT_SIZE', previousFontSize);
-		// AppConfig.common.GLOBAL.FONT_SIZE = parseInt(previousFontSize, 10);
-
 		this.toggleNavDrawerChanged.emit();
 	}
 
@@ -111,9 +80,6 @@ export class UserSettingsComponent implements OnChanges {
 		// Save all items
 		localStorage.setItem(AppConfig.visualizationCommon.GLOBAL.LS_ID + 'SETTING_NUMBER_PRECISION', this.numberPrecision);
 		AppConfig.visualizationCommon.GLOBAL.TO_FIXED = this.numberPrecision;
-
-		// localStorage.setItem(AppConfig.common.GLOBAL.LS_ID + 'LIFT_CURVE_SMOOTH', this.liftCurveSmooth.toString());
-		// AppConfig.common.LIFT_CURVE.LIFT_CURVE_SMOOTH = this.liftCurveSmooth;
 
 		localStorage.setItem(AppConfig.visualizationCommon.GLOBAL.LS_ID + 'FONT_SIZE', this.fontSize);
 		AppConfig.visualizationCommon.GLOBAL.FONT_SIZE = this.fontSize;
@@ -129,8 +95,7 @@ export class UserSettingsComponent implements OnChanges {
 		// this.khiopsLibraryService.trackEvent('click', 'settings', 'significant_number', this.numberPrecision);
 		// this.khiopsLibraryService.trackEvent('click', 'settings', 'matrix_contrast', this.contrastValue);
 
-		// TODO remove electron
-		// storage.setSync('COOKIE_CONSENT', this.allowCookies.toString());
+		localStorage.setItem('COOKIE_CONSENT_VISU', this.allowCookies.toString());
 
 		if (this.initialAllowCookies !== this.allowCookies) {
 			if (this.allowCookies === true) {
@@ -146,12 +111,6 @@ export class UserSettingsComponent implements OnChanges {
 
 	}
 
-	onFontSizeChanged(event) {
-		// this.fontSize = event.value;
-		// document.body.classList.remove('font-10', 'font-11', 'font-12', 'font-13', 'font-14', 'font-15', 'font-16', 'font-17', 'font-18');
-		// document.body.classList.add('font-' + event.value);
-	}
-
 	onClickOnClearDatas() {
 		localStorage.clear();
 		this.snackBar.open(this.translate.get('SNACKS.DATAS_DELETED'), null, {
@@ -159,12 +118,4 @@ export class UserSettingsComponent implements OnChanges {
 			panelClass: 'success'
 		});
 	}
-
-	// onLiftCurveSmoothChanged(event) {
-	// 	this.liftCurveSmooth = event.value;
-	// 	this.liftCurveSmoothExample = this.translate.get('SETTINGS.ONE_DATA_OUT_OF', {
-	// 		count: this.liftCurveSmooth.toString()
-	// 	});
-	// }
-
 }
