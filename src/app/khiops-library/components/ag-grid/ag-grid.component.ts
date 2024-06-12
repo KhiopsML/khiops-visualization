405834581e0d76e730bc1ae73c9af187b18d446f
Stéphane Bouget <stephane.bouget@orange.com>
2024-06-12 16:33:13 +0200

    Adapt styles to material 17 components

--- src/app/khiops-library/components/ag-grid/ag-grid.component.ts
+++ src/app/khiops-library/components/ag-grid/ag-grid.component.ts
@@ -528,6 +528,7 @@ export class AgGridComponent
 
   removeSearch() {
     this.searchInput = '';
+    console.log('removeSearch ~ this.searchInput :', this.searchInput );
     this.search();
     this.searchFormVisible = false;
   }
