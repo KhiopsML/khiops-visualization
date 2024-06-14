e631427c680f6c66a977c348aa5507593cca504f
Stéphane Bouget <stephane.bouget@orange.com>
2024-06-14 16:16:29 +0200

    remove checkCollapsedNodesIntegrity

--- src/tests/covisualization/app-service.spec.ts
+++ src/tests/covisualization/app-service.spec.ts
@@ -15,16 +15,16 @@ describe('coVisualization', () => {
       appService = TestBed.inject(AppService);
     });
 
-    it('checkCollapsedNodesIntegrity should return valid datas [collapsed nodes are leafs]', () => {
-      const fileDatas = require('../../assets/mocks/kc/h-Coclustering-2-2.json');
-      appService.setFileDatas(fileDatas);
-      let collapsedNodes: any = {
-        SepalLength: [']-inf;5.45]', ']5.45;+inf['],
-        SepalWidth: [']2.95;+inf['],
-      };
-      collapsedNodes = appService.checkCollapsedNodesIntegrity(collapsedNodes);
-      expect(collapsedNodes).toEqual({ SepalLength: [], SepalWidth: [] });
-    });
+    // it('checkCollapsedNodesIntegrity should return valid datas [collapsed nodes are leafs]', () => {
+    //   const fileDatas = require('../../assets/mocks/kc/h-Coclustering-2-2.json');
+    //   appService.setFileDatas(fileDatas);
+    //   let collapsedNodes: any = {
+    //     SepalLength: [']-inf;5.45]', ']5.45;+inf['],
+    //     SepalWidth: [']2.95;+inf['],
+    //   };
+    //   collapsedNodes = appService.checkCollapsedNodesIntegrity(collapsedNodes);
+    //   expect(collapsedNodes).toEqual({ SepalLength: [], SepalWidth: [] });
+    // });
 
     it('checkCollapsedNodesIntegrity should return valid datas [dimension name does not exist]', () => {
       const fileDatas = require('../../assets/mocks/kc/co-3-num.json');
@@ -37,14 +37,14 @@ describe('coVisualization', () => {
       expect(collapsedNodes).toEqual({ age: [']18.5;21.5]'] });
     });
 
-    it('checkCollapsedNodesIntegrity should return valid datas [node name does not exist]', () => {
-      const fileDatas = require('../../assets/mocks/kc/co-3-num.json');
-      appService.setFileDatas(fileDatas);
-      let collapsedNodes: any = {
-        age: [']brokenNodeName[', ']18.5;21.5]'],
-      };
-      collapsedNodes = appService.checkCollapsedNodesIntegrity(collapsedNodes);
-      expect(collapsedNodes).toEqual({ age: [']18.5;21.5]'] });
-    });
+    // it('checkCollapsedNodesIntegrity should return valid datas [node name does not exist]', () => {
+    //   const fileDatas = require('../../assets/mocks/kc/co-3-num.json');
+    //   appService.setFileDatas(fileDatas);
+    //   let collapsedNodes: any = {
+    //     age: [']brokenNodeName[', ']18.5;21.5]'],
+    //   };
+    //   collapsedNodes = appService.checkCollapsedNodesIntegrity(collapsedNodes);
+    //   expect(collapsedNodes).toEqual({ age: [']18.5;21.5]'] });
+    // });
   });
 });
