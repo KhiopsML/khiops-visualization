e4807850063a3dcebe2f91070d7aee5b2203a995
Stéphane Bouget <stephane.bouget@orange.com>
2024-06-24 16:28:02 +0200

    11.3.1

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -9,9 +9,9 @@ TAG=$1
 # Replace version into package.json
 sed -i "s/\"version\": \".*\"/\"version\": \"$TAG\"/" package.json
 
-# git add .
-# git commit -am "$TAG"
-# git tag v$TAG
+git add .
+git commit -am "$TAG"
+git tag v$TAG
 # git push origin master --tags
 
 # exit 0
