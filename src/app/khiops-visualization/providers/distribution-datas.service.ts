ae80408b2c7373033fc774ebc0973624c2328f9d
Stéphane Bouget <stephane.bouget@orange.com>
2024-10-21 15:32:09 +0200

    Improve DistributionDatasService code organization and readability

--- src/app/khiops-visualization/providers/distribution-datas.service.ts
+++ src/app/khiops-visualization/providers/distribution-datas.service.ts
@@ -35,46 +35,56 @@ export class DistributionDatasService {
     this.initialize();
   }
 
+  /**
+   * Initializes the distribution data by fetching the application data
+   * from the appService and creating a new instance of DistributionDatasModel
+   * with the fetched data.
+   *
+   * @remarks
+   * This method should be called to set up the distribution data required
+   * for the component.
+   */
   initialize() {
     const appDatas = this.appService.getDatas().datas;
     this.distributionDatas = new DistributionDatasModel(appDatas);
   }
 
+  /**
+   * Retrieves the distribution data.
+   *
+   * @returns {DistributionDatasModel} The distribution data model.
+   */
   getDatas(): DistributionDatasModel {
     return this.distributionDatas;
   }
 
+  /**
+   * Sets the preparation source for the distribution data.
+   *
+   * @param preparationSource - The source of the preparation to be set.
+   */
   setPreparationSource(preparationSource: string) {
     this.distributionDatas.preparationSource = preparationSource;
   }
 
+  /**
+   * Sets the displayed values for the target distribution.
+   *
+   * @param values - An array of `ChartToggleValuesI` representing the values to be displayed.
+   */
   setTargetDistributionDisplayedValues(values: ChartToggleValuesI[]) {
     this.distributionDatas.targetDistributionDisplayedValues = values;
   }
 
-  computeModalityCounts(modality): ModalityCountsModel {
-    const counts = new ModalityCountsModel();
-    if (modality) {
-      const dimensionLength = modality[0].length;
-      for (let i = 0; i < modality.length; i++) {
-        for (let j = 0; j < dimensionLength; j++) {
-          if (!counts.series[j]) {
-            counts.series[j] = 0;
-          }
-          counts.series[j] = counts.series[j] + modality[i][j];
-          counts.total = counts.total + modality[i][j];
-        }
-      }
-      for (let k = 0; k < dimensionLength; k++) {
-        counts.totalProbability[k] =
-          counts.series[k] / counts.series.reduce((a, b) => a + b, 0);
-      }
-    }
-
-    return counts;
-  }
-
-  // tslint:disable-next-line:typedef-whitespace
+  /**
+   * Retrieves and processes the target distribution graph data for a given variable.
+   *
+   * @param selectedVariable - The variable for which the target distribution graph data is to be retrieved.
+   *                           It can be either a `PreparationVariableModel` or a `TreePreparationVariableModel`.
+   * @param type - (Optional) The type of distribution to be set.
+   * @param initActiveEntries - (Optional) A flag to initialize active entries. Defaults to `true` if not provided.
+   * @returns The processed target distribution graph data as a `ChartDatasModel`.
+   */
   getTargetDistributionGraphDatas(
     selectedVariable: PreparationVariableModel | TreePreparationVariableModel,
     type?: string,
@@ -121,6 +131,20 @@ export class DistributionDatasService {
     return this.distributionDatas.targetDistributionGraphDatas;
   }
 
+  /**
+   * Retrieves and computes the target distribution graph data for a given tree node.
+   *
+   * @param {TreeNodeModel} selectedNode - The selected tree node for which the target distribution graph data is to be retrieved.
+   * @param {string} [type] - An optional parameter specifying the type of distribution.
+   * @returns {ChartDatasModel} - The computed target distribution graph data.
+   *
+   * This method initializes the target distribution graph data and sets the distribution type.
+   * It then retrieves the application data and the selected variable. If the preparation source,
+   * selected variable, and selected node (which must be a leaf) are valid, it computes the full
+   * target values and updates the current data, filling empty values with 0. It also retrieves
+   * the variable details and computes the target distribution graph data using the provided
+   * parameters. Finally, it checks the computed graph data and returns it.
+   */
   getTreeNodeTargetDistributionGraphDatas(
     selectedNode: TreeNodeModel,
     type?: string,
@@ -181,6 +205,18 @@ export class DistributionDatasService {
     return this.distributionDatas.treeNodeTargetDistributionGraphDatas;
   }
 
+  /**
+   * Computes the target distribution graph data based on the provided parameters.
+   *
+   * @param partition - An array representing the partition of data, which can be either strings or numbers.
+   * @param currentDatas - A 2D array of numbers representing the current data set.
+   * @param allDatas - A 2D array of numbers representing all data sets.
+   * @param currentXAxis - An array representing the current X-axis values, which can be strings or numbers.
+   * @param displayedValues - An optional array of `ChartToggleValuesI` representing the values to be displayed on the graph.
+   * @param type - A string representing the type of graph to be computed (e.g., probabilities or lift).
+   * @param selectedVariableType - A string representing the type of the selected variable.
+   * @returns A tuple containing the computed `ChartDatasModel` and an optional array of `ChartToggleValuesI`.
+   */
   computeTargetDistributionGraph(
     partition: string[] | number[][],
     currentDatas: number[][],
@@ -212,7 +248,9 @@ export class DistributionDatasService {
 
       for (let k = 0; k < dimensionLength; k++) {
         const currentPartition = partition[k];
-        const currentDataSet = new ChartDatasetModel(currentPartition.toString());
+        const currentDataSet = new ChartDatasetModel(
+          currentPartition.toString(),
+        );
         targetDistributionGraphDatas.datasets.push(currentDataSet);
 
         let l: number = currentXAxis.length;
@@ -264,7 +302,14 @@ export class DistributionDatasService {
     return [targetDistributionGraphDatas, displayedValues];
   }
 
-  // tslint:disable-next-line:typedef-whitespace
+  /**
+   * Retrieves and computes the distribution graph data for a selected variable.
+   *
+   * @param selectedVariable - The variable for which the distribution graph data is to be retrieved.
+   * @param type - (Optional) The type of distribution. If provided, it sets the distribution type.
+   * @param initActiveEntries - (Optional) A boolean indicating whether to initialize active entries. Defaults to true if not provided.
+   * @returns A `ChartDatasModel` object containing the computed distribution graph data. If no valid data is found, returns `undefined`.
+   */
   getdistributionGraphDatas(
     selectedVariable,
     type?: string,
@@ -325,6 +370,16 @@ export class DistributionDatasService {
     return distributionsGraphDetails;
   }
 
+  /**
+   * Retrieves histogram graph data for a selected variable.
+   *
+   * This method processes the detailed statistics of a selected variable from the application data
+   * and generates histogram graph details. It calculates the frequency, value, and log value for
+   * each partition of the variable's data grid.
+   *
+   * @param selectedVariable - The variable for which histogram data is to be generated. It should have a `rank` property.
+   * @returns An array of `HistogramValuesI` objects containing the histogram graph details, or `undefined` if the data is not available.
+   */
   getHistogramGraphDatas(selectedVariable): HistogramValuesI[] | undefined {
     const appDatas = this.appService.getDatas().datas;
     const varDatas =
@@ -367,6 +422,17 @@ export class DistributionDatasService {
     return histogramGraphDetails;
   }
 
+  /**
+   * Computes the distribution graph based on the provided parameters.
+   *
+   * @param currentDimension - The current dimension object containing partition and default group index.
+   * @param currentDatas - The current data set to be processed.
+   * @param dimensions - The dimensions to be considered for the distribution.
+   * @param partition - The partition value for the current dimension.
+   * @param currentXAxis - The current X-axis values.
+   * @param selectedVariable - The selected variable containing type information.
+   * @returns The computed distribution graph details or undefined if no datasets are available.
+   */
   computeDistributionGraph(
     currentDimension,
     currentDatas,
@@ -443,6 +509,16 @@ export class DistributionDatasService {
     return distributionsGraphDetails;
   }
 
+  /**
+   * Computes the frequency and coverage values for the given data, dimensions, and partition.
+   *
+   * @param currentDatas - The array of data items to process.
+   * @param dimensions - The dimensions to consider for coverage calculation.
+   * @param partition - The partition to consider for coverage calculation.
+   * @returns A tuple containing two arrays:
+   *   - The first array contains the frequency values (logarithm of coverage values).
+   *   - The second array contains the coverage values.
+   */
   getAllFrequencyAndCoverageValues(currentDatas, dimensions, partition) {
     const frequencyArray: number[] = [];
     const coverageArray: number[] = [];
@@ -459,6 +535,14 @@ export class DistributionDatasService {
     return [frequencyArray, coverageArray];
   }
 
+  /**
+   * Computes the coverage value based on the given dimensions, partition, and element.
+   *
+   * @param dimensions - An array representing the dimensions.
+   * @param partition - The partition value used in the computation.
+   * @param el - The element used to compute the coverage value. It can be a number or an array of numbers.
+   * @returns The computed coverage value.
+   */
   getCoverageValueFromDimensionAndPartition(dimensions, partition, el) {
     let coverageValue = 0;
     // compute total
@@ -478,6 +562,13 @@ export class DistributionDatasService {
     return coverageValue;
   }
 
+  /**
+   * Generates level distribution graph data from the provided input data.
+   *
+   * @param inputDatas - The input data array containing objects with level information.
+   * @param limit - An optional limit on the number of variables to include in the graph. Defaults to 0.
+   * @returns A `ChartDatasModel` object containing the datasets and labels for the level distribution graph.
+   */
   getLeveldistributionGraphDatas(inputDatas: any, limit = 0): ChartDatasModel {
     const levelDistributionGraphDatas: ChartDatasModel = new ChartDatasModel();
 
@@ -508,6 +599,14 @@ export class DistributionDatasService {
     return levelDistributionGraphDatas;
   }
 
+  /**
+   * Formats the X-axis label based on the provided partition type and index.
+   *
+   * @param currentXAxis - The current X-axis value to be formatted.
+   * @param index - The index of the current X-axis value.
+   * @param partitionType - The type of partition, either 'Numerical' or 'Categorical'.
+   * @returns The formatted X-axis label as a string.
+   */
   formatXAxis(currentXAxis: any, index: number, partitionType: string): string {
     let currentName: string;
 
@@ -555,5 +654,36 @@ export class DistributionDatasService {
     }
 
     return currentName;
+  }
+
+  /**
+   * Computes the modality counts for a given modality.
+   *
+   * @param modality - A 2D array representing the modality data.
+   * @returns An instance of `ModalityCountsModel` containing the computed counts, total, and total probabilities.
+   *
+   * The method iterates through the modality data to calculate the series counts and total counts.
+   * It then computes the total probability for each dimension.
+   */
+  computeModalityCounts(modality): ModalityCountsModel {
+    const counts = new ModalityCountsModel();
+    if (modality) {
+      const dimensionLength = modality[0].length;
+      for (let i = 0; i < modality.length; i++) {
+        for (let j = 0; j < dimensionLength; j++) {
+          if (!counts.series[j]) {
+            counts.series[j] = 0;
+          }
+          counts.series[j] = counts.series[j] + modality[i][j];
+          counts.total = counts.total + modality[i][j];
+        }
+      }
+      for (let k = 0; k < dimensionLength; k++) {
+        counts.totalProbability[k] =
+          counts.series[k] / counts.series.reduce((a, b) => a + b, 0);
+      }
+    }
+
+    return counts;
   }
 }
