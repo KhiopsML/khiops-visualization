96ac00c162f55ed2ae8af5f9f1deb25b1dfa7528
Stéphane Bouget <stephane.bouget@orange.com>
2025-04-23 09:36:45 +0200

    Adjust default column sizes in VIEW_LAYOUT

--- src/app/khiops-covisualization/config/view-layout.ts
+++ src/app/khiops-covisualization/config/view-layout.ts
@@ -9,14 +9,14 @@ export const VIEW_LAYOUT = {
     col: [70, 30],
     col0Row: [50, 50],
     col1Row: [20, 80],
-    col0Row0Col: [25, 25, 25, 25],
+    col0Row0Col: [20, 20, 20, 20, 20],
     col0Row0Col2Row: [50, 50],
-    col0Row1Col: [25, 25, 25, 25],
+    col0Row1Col: [20, 20, 20, 20, 20],
     col0Row1Col2Row: [30, 70],
   },
   contextView: {
     col: [70, 30],
-    col0Row0Col: [25, 25, 25, 25],
+    col0Row0Col: [20, 20, 20, 20, 20],
     col0Row0Col2Row: [30, 70],
   },
 };
