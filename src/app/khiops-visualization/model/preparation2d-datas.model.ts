a9564109a2951caa8a63393408f1125c4aabedcf
Stéphane Bouget <stephane.bouget@orange.com>
2025-07-09 11:35:29 +0200

    Refactor Preparation2dDatasModel and Preparation2dDatasService to remove appDatas dependency and enhance validation logic

--- src/app/khiops-visualization/model/preparation2d-datas.model.ts
+++ src/app/khiops-visualization/model/preparation2d-datas.model.ts
@@ -8,7 +8,6 @@ import { CellModel } from '@khiops-library/model/cell.model';
 import { Preparation2dVariableModel } from './preparation2d-variable.model';
 import { GridColumnsI } from '@khiops-library/interfaces/grid-columns';
 import { MatrixDatasModel } from '@khiops-library/model/matrix-datas.model';
-import { VisualizationDatas } from '@khiops-visualization/interfaces/app-datas';
 
 export class Preparation2dDatasModel {
   isAxisInverted = false;
@@ -21,34 +20,5 @@ export class Preparation2dDatasModel {
     values: any[][]; // Dynamic values according to the input datas
     displayedColumns: GridColumnsI[][];
   };
-
-  appDatas?: VisualizationDatas = undefined;
   isSupervised = false;
-
-  constructor(appDatas: VisualizationDatas) {
-    this.appDatas = appDatas;
-    this.isSupervised = this.isSupervisedVariable();
-  }
-
-  /**
-   * Check if current datas are valid
-   */
-  isValid(): boolean {
-    return this.appDatas?.bivariatePreparationReport
-      ?.variablesPairsStatistics?.[0]
-      ? true
-      : false;
-  }
-
-  isSupervisedVariable(): boolean {
-    return (
-      this.appDatas?.bivariatePreparationReport
-        ?.variablesPairsDetailedStatistics?.[
-        Object.keys(
-          this.appDatas?.bivariatePreparationReport
-            ?.variablesPairsDetailedStatistics,
-        )?.[0]!
-      ]?.dataGrid?.isSupervised || false
-    );
-  }
 }
