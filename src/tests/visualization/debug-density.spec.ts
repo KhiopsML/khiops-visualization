7cc630a14aa45e1fb3f61090f4d32c9a65b3cdd4
Stéphane Bouget <stephane.bouget@orange.com>
2025-07-24 10:26:52 +0200

    test: add debug test for density calculation in HistogramService

---
+++ src/tests/visualization/debug-density.spec.ts
@@ -0,0 +1,68 @@
+// @ts-nocheck
+/*
+ * Debug test to understand the density calculation issue
+ */
+
+import { TestBed } from '@angular/core/testing';
+import { HistogramService } from '../../app/khiops-visualization/components/commons/histogram/histogram.service';
+import { HistogramValuesI } from '../../app/khiops-visualization/components/commons/histogram/histogram.interfaces';
+
+describe('Debug Density Calculation', () => {
+  let histogramService: HistogramService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [HistogramService],
+    });
+    histogramService = TestBed.inject(HistogramService);
+  });
+
+  it('should debug density calculation with large values', () => {
+    // Simuler des données avec de très grandes densités comme dans le bug report
+    const testData: HistogramValuesI[] = [
+      {
+        frequency: 8,
+        logValue: 3.93,
+        partition: [0.4956450674, 0.4965820313],
+        density: 8542.52,
+        probability: 0.00255102,
+      },
+      {
+        frequency: 1243,
+        logValue: 6.008,
+        partition: [0.4995117188, 0.5007324219],
+        density: 1018265.62, // Très grande valeur
+        probability: 0.396365,
+      },
+      {
+        frequency: 387,
+        logValue: 4.9,
+        partition: [0.5007324219, 0.5012207031],
+        density: 79357.94,
+        probability: 0.123406,
+      },
+    ];
+
+    const rangeYLin = histogramService.getLinRangeY(testData);
+    console.log('Range Y Lin (max density):', rangeYLin);
+
+    const canvasHeight = 400;
+    const yPadding = 50;
+    const ratioY = histogramService.getLinRatioY(canvasHeight, yPadding);
+    console.log('Ratio Y:', ratioY);
+
+    testData.forEach((data, index) => {
+      const calculatedHeight = data.density * ratioY;
+      console.log(`Data ${index}:`, {
+        density: data.density,
+        calculatedHeight: calculatedHeight,
+        maxAllowedHeight: canvasHeight - yPadding / 2,
+        wouldExceed: calculatedHeight > canvasHeight - yPadding / 2
+      });
+    });
+
+    // Vérifier que les calculs sont corrects
+    expect(rangeYLin).toEqual(1018265.62);
+    expect(ratioY).toBeCloseTo((canvasHeight - yPadding / 2) / rangeYLin);
+  });
+});
