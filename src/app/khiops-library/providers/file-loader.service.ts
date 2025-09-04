0173b26f1fd82098788bb50d723b692676980101
Stéphane Bouget <stephane.bouget@orange.com>
2025-09-04 09:57:37 +0200

    Add mock file configuration for visualization and co-visualization debugging

--- src/app/khiops-library/providers/file-loader.service.ts
+++ src/app/khiops-library/providers/file-loader.service.ts
@@ -10,6 +10,10 @@ import { KhiopsLibraryService } from './khiops-library.service';
 import { Ls } from '@khiops-library/providers/ls.service';
 import { LS } from '@khiops-library/enum/ls';
 import { BehaviorSubject, Observable } from 'rxjs';
+import {
+  getVisualizationMockFile,
+  getCoviszalizationMockFile,
+} from './mock-files.config';
 
 @Injectable({
   providedIn: 'root',
@@ -66,128 +70,16 @@ export class FileLoaderService {
 
   /**
    * Debugs the reading of visualization files.
+   * File selection is configured in mock-files.config.ts - uncomment the desired file there.
    *
-   * @param fileName - The name of the file to read.
+   * @param fileName - The name of the file to read. If not provided, uses the file from configuration.
    * @returns The file data or an error message.
    */
   debugReadDatas(fileName?: string): any {
-    // Visualization files
-    // ===================
-    //
-
-    // let urlKV = './assets/mocks/kv/new-hyper-tree.json';
-    // let urlKV = './assets/mocks/kv/NGrams10_AnalysisResults.json';
-    // let urlKV = './assets/mocks/kv/NGrams100_AnalysisResults.json';
-    // let urlKV = './assets/mocks/kv/000_000_10000words_AllReports.json';
-    // let urlKV = './assets/mocks/kv/Regression_AllReports_All.json';
-    // let urlKV = './assets/mocks/kv/Regression_AllReports_PreparationOnly.json';
-    // let urlKV = './assets/mocks/kv/missing-zero.json';
-    // let urlKV = './assets/mocks/kv/C1000_AllReports.json';
-    // let urlKV = './assets/mocks/kv/C100_AllReports.json';
-    // let urlKV = './assets/mocks/kv/AdultRegressionAllReports.json';
-    // let urlKV = './assets/mocks/kv/onlyEvaluationReport.json';
-    // let urlKV = './assets/mocks/kv/explanatory.json';
-    // let urlKV = './assets/mocks/kv/adult-bivar.json';
-    // let urlKV = './assets/mocks/kv/desc-bivar.json';
-    // let urlKV = './assets/mocks/kv/irisR.json';
-    // let urlKV = './assets/mocks/kv/bi2.json';
-    // let urlKV = './assets/mocks/kv/bi3.json';
-    // let urlKV = './assets/mocks/kv/Std_Iris_AnalysisResults.khj';
-    // let urlKV = './assets/mocks/kv/C1_AllReports.json';
-    // let urlKV = './assets/mocks/kv/C0_AllReports.json';
-    // let urlKV = './assets/mocks/kv/analyse_supervisee_multiclasse.json';
-    // let urlKV = './assets/mocks/kv/reg.json';
-    // let urlKV = './assets/mocks/kv/copydatas.json';
-    // let urlKV = './assets/mocks/kv/iris2d.json';
-    let urlKV = './assets/mocks/kv/BugAnalysisResults.khj';
-    // let urlKV = './assets/mocks/kv/BugAnalysisResults_NewBounds.khj';
-    // let urlKV = './assets/mocks/kv/AnalysisRegressionQ99.json';
-    // let urlKV = './assets/mocks/kv/AnalysisResults.json';
-    // let urlKV = './assets/mocks/kv/test_report.json';
-    // let urlKV = './assets/mocks/kv/mainTargetValue.json';
-    // let urlKV = './assets/mocks/kv/leafrules.khj';
-    // let urlKV = './assets/mocks/kv/Filtered_Iris_AnalysisResults.khj';
-    // let urlKV = './assets/mocks/kv/defaultGroup.json';
-    // let urlKV = './assets/mocks/kv/AdultAgeAllReports.json';
-    // let urlKV = './assets/mocks/kv/missing-level-parts.json';
-    // let urlKV = './assets/mocks/kv/lift-issue.khj';
-    // let urlKV = './assets/mocks/kv/tree-education_AllReports.json';
-    // let urlKV = './assets/mocks/kv/incomplete-detailed-stats.json';
-    // let urlKV = './assets/mocks/kv/OI_AllReports.json';
-    // let urlKV = './assets/mocks/kv/tree.json';
-    // let urlKV = './assets/mocks/kv/Natives_AllReports.json';
-    // let urlKV = './assets/mocks/kv/2d-cells-AllReports.json';
-    // let urlKV = './assets/mocks/kv/bigTreeLeafs.json';
-    // let urlKV = './assets/mocks/kv/AnalysisResults_regtree_iris.json';
-    // let urlKV = './assets/mocks/kv/level.json';
-    // let urlKV = './assets/mocks/kv/xor.json';
-    // let urlKV = './assets/mocks/kv/Natives-Arbres_Paires_AllReports.json';
-    // let urlKV = './assets/mocks/kv/co-oc.json';
-    // let urlKV = './assets/mocks/kv/MSE_AllReports.json';
-    // let urlKV = './assets/mocks/kv/marc.json';
-    // let urlKV = './assets/mocks/kv/marc2.json';
-    // let urlKV = './assets/mocks/kv/tree-AllReports.json';
-    // let urlKV = './assets/mocks/kv/test_long_XDSL_Delc_AllReports.json';
-    // let urlKV = './assets/mocks/kv/Essai_1_AllReports.json';
-    // let urlKV = './assets/mocks/kv/typeBotnet_AllReports.json';
-    // let urlKV = './assets/mocks/kv/irisU.json';
-    // let urlKV = './assets/mocks/kv/CoronaWords100000_AllReports.V11.json';
-    // let urlKV = './assets/mocks/kv/CrirteoAllReports.json';
-    // let urlKV = './assets/mocks/kv/CriteoAnalysisResults.khj';
-    // let urlKV = './assets/mocks/kv/Words100_AllReports.json';
-    // let urlKV = './assets/mocks/kv/UnivariateAnalysisResults.json';
-    // let urlKV = './assets/mocks/kv/ylogAdultAllReports.json';
-    // let urlKV = './assets/mocks/kv/AdversePairAnalysisResults.Bug.khj';
-    // let urlKV = './assets/mocks/kv/AdversePairAnalysisResults.NoSNB.khj';
-    // let urlKV = './assets/mocks/kv/AdversePairAnalysisResults.Corrected.khj';
-    // let urlKV = './assets/mocks/kv/ko-AnalysisResults.khj';
-    // let urlKV = './assets/mocks/kv/AdultAllReports.json';
-    // let urlKV = './assets/mocks/kv/20NewsgroupAllReports.json';
-
-    // Co visualization files
-    // ======================
-    //
-    let urlKC = './assets/mocks/kc/Coclustering-100x100.json';
-    // let urlKC = './assets/mocks/kc/IV-Poissons.json';
-    // let urlKC = './assets/mocks/kc/cc.json';
-    // let urlKC = './assets/mocks/kc/Coclustering.json';
-    // let urlKC = './assets/mocks/kc/Co-simple-2vars.json';
-    // let urlKC = './assets/mocks/kc/Coclustering-6.json';
-    // let urlKC = './assets/mocks/kc/Coclustering-4.json';
-    // let urlKC = './assets/mocks/kc/1-Adult2varsEducationOccupation.khcj';
-    // let urlKC = './assets/mocks/kc/2-Iris3vars-Coclustering.khcj';
-    // let urlKC = './assets/mocks/kc/co-IrisMissing.json';
-    // let urlKC = './assets/mocks/kc/adult2var.json';
-    // let urlKC = './assets/mocks/kc/sample3.json';
-    // let urlKC = './assets/mocks/kc/DataNoisyCorrelatedN1000000_C1000_V10_L5Coclustering.json';
-    // let urlKC = './assets/mocks/kc/sample0.json';
-    // let urlKC = './assets/mocks/kc/WebSpamCoclustering18mb.json';
-    // let urlKC = './assets/mocks/kc/adultmissing.json';
-    // let urlKC = './assets/mocks/kc/DigitCoclustering.json';
-    // let urlKC = './assets/mocks/kc/8-TS4624User_Coclustering.json';
-    // let urlKC = './assets/mocks/kc/IV-AdultEducation.json';
-    // let urlKC = './assets/mocks/kc/Coclustering-IVAdultEducation.json';
-    // let urlKC = './assets/mocks/kc/Coclustering-IV-Glass.json';
-    // let urlKC = './assets/mocks/kc/Coclustering-IV-Iris.khcj';
-    // let urlKC = './assets/mocks/kc/Coclustering-IV-Pima.khcj';
-    // let urlKC = './assets/mocks/kc/Coclustering_NewSeparator.khcj';
-    // let urlKC = './assets/mocks/kc/check-ext-datas.json';
-    // let urlKC = './assets/mocks/kc/mushroom.json';
-    // let urlKC = './assets/mocks/kc/SimplifiedCoclusteringIrisOldFormat_BugUndefined.json';
-    // let urlKC = './assets/mocks/kc/h-Coclustering.json';
-    // let urlKC = './assets/mocks/kc/irismissing.json';
-    // let urlKC = './assets/mocks/kc/zero-except.json';
-    // let urlKC = './assets/mocks/kc/AdultSmall.json';
-    // let urlKC = './assets/mocks/kc/VerbNounCoclustering27mo.json';
-    // let urlKC = './assets/mocks/kc/v4.json';
-    // let urlKC = './assets/mocks/kc/NovaCoclustering4mb.json';
-    // let urlKC =
-    //   './assets/mocks/kc/10.1.1_id_feat_nospace_Coclustering.json';
-    // let urlKC = './assets/mocks/kc/co-3-num.json';
-    // let urlKC = './assets/mocks/kc/ext-CC_Coclustering.json';
-    // let urlKC = './assets/mocks/kc/ext-datas_Coclustering.json';
-    // let urlKC = './assets/mocks/kc/ext-CC_Coclustering.json';
-    // let urlKC = './assets/mocks/kc/CC_3_Coclustering.json';
+    // Visualization files and Co visualization files
+    // File selection is configured in mock-files.config.ts
+    let urlKV = getVisualizationMockFile();
+    let urlKC = getCoviszalizationMockFile();
 
     if (fileName) {
       urlKV = './assets/mocks/kv/' + fileName;
@@ -196,9 +88,9 @@ export class FileLoaderService {
 
     let url;
     if (this.khiopsLibraryService.isKhiopsVisu()) {
-      url = urlKV;
+      url = './assets/mocks/kv/' + urlKV;
     } else {
-      url = urlKC;
+      url = './assets/mocks/kc/' + urlKC;
     }
     return this.readWebFile(url);
   }
---
+++ src/app/khiops-library/providers/mock-files.config.ts
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2023-2025 Orange. All rights reserved.
+ * This software is distributed under the BSD 3-Clause-clear License, the text of which is available
+ * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
+ */
+
+/**
+ * Mock files for visualization (KV) debugging.
+ * To change the default file, uncomment the desired line and comment the current active one.
+ */
+export function getVisualizationMockFile(): string {
+  let urlKV = 'new-hyper-tree.json';
+  // let urlKV = 'NGrams10_AnalysisResults.json';
+  // let urlKV = 'NGrams100_AnalysisResults.json';
+  // let urlKV = '000_000_10000words_AllReports.json';
+  // let urlKV = 'Regression_AllReports_All.json';
+  // let urlKV = 'Regression_AllReports_PreparationOnly.json';
+  // let urlKV = 'missing-zero.json';
+  // let urlKV = 'C1000_AllReports.json';
+  // let urlKV = 'C100_AllReports.json';
+  // let urlKV = 'AdultRegressionAllReports.json';
+  // let urlKV = 'onlyEvaluationReport.json';
+  // let urlKV = 'explanatory.json';
+  // let urlKV = 'adult-bivar.json';
+  // let urlKV = 'desc-bivar.json';
+  // let urlKV = 'irisR.json';
+  // let urlKV = 'bi2.json';
+  // let urlKV = 'bi3.json';
+  // let urlKV = 'Std_Iris_AnalysisResults.khj';
+  // let urlKV = 'C1_AllReports.json';
+  // let urlKV = 'C0_AllReports.json';
+  // let urlKV = 'analyse_supervisee_multiclasse.json';
+  // let urlKV = 'reg.json';
+  // let urlKV = 'copydatas.json';
+  // let urlKV = 'iris2d.json';
+  // let urlKV = 'BugAnalysisResults.khj';
+  // let urlKV = 'BugAnalysisResults_NewBounds.khj';
+  // let urlKV = 'AnalysisRegressionQ99.json';
+  // let urlKV = 'AnalysisResults.json';
+  // let urlKV = 'test_report.json';
+  // let urlKV = 'mainTargetValue.json';
+  // let urlKV = 'leafrules.khj';
+  // let urlKV = 'Filtered_Iris_AnalysisResults.khj';
+  // let urlKV = 'defaultGroup.json';
+  // let urlKV = 'AdultAgeAllReports.json';
+  // let urlKV = 'missing-level-parts.json';
+  // let urlKV = 'lift-issue.khj';
+  // let urlKV = 'tree-education_AllReports.json';
+  // let urlKV = 'incomplete-detailed-stats.json';
+  // let urlKV = 'OI_AllReports.json';
+  // let urlKV = 'tree.json';
+  // let urlKV = 'Natives_AllReports.json';
+  // let urlKV = '2d-cells-AllReports.json';
+  // let urlKV = 'bigTreeLeafs.json';
+  // let urlKV = 'AnalysisResults_regtree_iris.json';
+  // let urlKV = 'level.json';
+  // let urlKV = 'xor.json';
+  // let urlKV = 'Natives-Arbres_Paires_AllReports.json';
+  // let urlKV = 'co-oc.json';
+  // let urlKV = 'MSE_AllReports.json';
+  // let urlKV = 'marc.json';
+  // let urlKV = 'marc2.json';
+  // let urlKV = 'tree-AllReports.json';
+  // let urlKV = 'test_long_XDSL_Delc_AllReports.json';
+  // let urlKV = 'Essai_1_AllReports.json';
+  // let urlKV = 'typeBotnet_AllReports.json';
+  // let urlKV = 'irisU.json';
+  // let urlKV = 'CoronaWords100000_AllReports.V11.json';
+  // let urlKV = 'CrirteoAllReports.json';
+  // let urlKV = 'CriteoAnalysisResults.khj';
+  // let urlKV = 'Words100_AllReports.json';
+  // let urlKV = 'UnivariateAnalysisResults.json';
+  // let urlKV = 'ylogAdultAllReports.json';
+  // let urlKV = 'AdversePairAnalysisResults.Bug.khj';
+  // let urlKV = 'AdversePairAnalysisResults.NoSNB.khj';
+  // let urlKV = 'AdversePairAnalysisResults.Corrected.khj';
+  // let urlKV = 'ko-AnalysisResults.khj';
+  // let urlKV = 'AdultAllReports.json';
+  // let urlKV = '20NewsgroupAllReports.json';
+
+  return urlKV;
+}
+
+/**
+ * Mock files for co-visualization (KC) debugging.
+ * To change the default file, uncomment the desired line and comment the current active one.
+ */
+export function getCoviszalizationMockFile(): string {
+  let urlKC = 'Coclustering-100x100.json';
+  // let urlKC = 'IV-Poissons.json';
+  // let urlKC = 'cc.json';
+  // let urlKC = 'Coclustering.json';
+  // let urlKC = 'Co-simple-2vars.json';
+  // let urlKC = 'Coclustering-6.json';
+  // let urlKC = 'Coclustering-4.json';
+  // let urlKC = '1-Adult2varsEducationOccupation.khcj';
+  // let urlKC = '2-Iris3vars-Coclustering.khcj';
+  // let urlKC = 'co-IrisMissing.json';
+  // let urlKC = 'adult2var.json';
+  // let urlKC = 'sample3.json';
+  // let urlKC = 'DataNoisyCorrelatedN1000000_C1000_V10_L5Coclustering.json';
+  // let urlKC = 'sample0.json';
+  // let urlKC = 'WebSpamCoclustering18mb.json';
+  // let urlKC = 'adultmissing.json';
+  // let urlKC = 'DigitCoclustering.json';
+  // let urlKC = '8-TS4624User_Coclustering.json';
+  // let urlKC = 'IV-AdultEducation.json';
+  // let urlKC = 'Coclustering-IVAdultEducation.json';
+  // let urlKC = 'Coclustering-IV-Glass.json';
+  // let urlKC = 'Coclustering-IV-Iris.khcj';
+  // let urlKC = 'Coclustering-IV-Pima.khcj';
+  // let urlKC = 'Coclustering_NewSeparator.khcj';
+  // let urlKC = 'check-ext-datas.json';
+  // let urlKC = 'mushroom.json';
+  // let urlKC = 'SimplifiedCoclusteringIrisOldFormat_BugUndefined.json';
+  // let urlKC = 'h-Coclustering.json';
+  // let urlKC = 'irismissing.json';
+  // let urlKC = 'zero-except.json';
+  // let urlKC = 'AdultSmall.json';
+  // let urlKC = 'VerbNounCoclustering27mo.json';
+  // let urlKC = 'v4.json';
+  // let urlKC = 'NovaCoclustering4mb.json';
+  // let urlKC = '10.1.1_id_feat_nospace_Coclustering.json';
+  // let urlKC = 'co-3-num.json';
+  // let urlKC = 'ext-CC_Coclustering.json';
+  // let urlKC = 'ext-datas_Coclustering.json';
+  // let urlKC = 'ext-CC_Coclustering.json';
+  // let urlKC = 'CC_3_Coclustering.json';
+
+  return urlKC;
+}
