d6e3adfa466975042991bbf12fbe2a9640cca28a
Stéphane Bouget <stephane.bouget@orange.com>
2025-09-11 14:40:52 +0200

    Refactor target lift graph component and service

--- src/app/khiops-visualization/components/commons/target-lift-graph/target-lift-graph.component.ts
+++ src/app/khiops-visualization/components/commons/target-lift-graph/target-lift-graph.component.ts
@@ -13,23 +13,20 @@ import {
   ViewChild,
 } from '@angular/core';
 import { EvaluationDatasService } from '@khiops-visualization/providers/evaluation-datas.service';
-import { KhiopsLibraryService } from '@khiops-library/providers/khiops-library.service';
 import { TranslateService } from '@ngstack/translate';
 import { SelectableComponent } from '@khiops-library/components/selectable/selectable.component';
 import { SelectableService } from '@khiops-library/components/selectable/selectable.service';
-import _ from 'lodash';
 import { ChartColorsSetI } from '@khiops-library/interfaces/chart-colors-set';
 import { ConfigService } from '@khiops-library/providers/config.service';
-import { ChartOptions, TooltipItem } from 'chart.js';
+import { ChartOptions } from 'chart.js';
 import { ChartDatasModel } from '@khiops-library/model/chart-datas.model';
 import { EvaluationDatasModel } from '@khiops-visualization/model/evaluation-datas.model';
 import { TargetLiftValuesI } from '@khiops-visualization/interfaces/target-lift-values';
 import { COMPONENT_TYPES } from '@khiops-library/enum/component-types';
-import { LS } from '@khiops-library/enum/ls';
-import { AppService } from '@khiops-visualization/providers/app.service';
 import { EvaluationPredictorModel } from '@khiops-visualization/model/evaluation-predictor.model';
 import { ChartToggleValuesI } from '@khiops-visualization/interfaces/chart-toggle-values';
 import { MatMenuTrigger } from '@angular/material/menu';
+import { TargetLiftGraphService } from './target-lift-graph.service';
 
 @Component({
   selector: 'app-target-lift-graph',
@@ -63,74 +60,20 @@ export class TargetLiftGraphComponent
     public override configService: ConfigService,
     private evaluationDatasService: EvaluationDatasService,
     private translate: TranslateService,
-    private khiopsLibraryService: KhiopsLibraryService,
+    private targetLiftGraphService: TargetLiftGraphService,
   ) {
     super(selectableService, ngzone, configService);
     this.evaluationDatas = this.evaluationDatasService.getDatas();
 
-    this.legendColorSet = _.cloneDeep(
-      this.khiopsLibraryService.getGraphColorSet()[1],
-    );
-    this.colorSet = _.cloneDeep(
-      this.khiopsLibraryService.getGraphColorSet()[1],
-    );
-
-    let xAxisLabel: string;
-    let yAxisLabel: string;
-    if (this.evaluationDatasService.isRegressionAnalysis()) {
-      xAxisLabel = this.translate.get('GLOBAL.RANK_ERROR') + ' %';
-      yAxisLabel = this.translate.get('GLOBAL.POPULATION') + ' %';
-    } else {
-      xAxisLabel = this.translate.get('GLOBAL.POPULATION') + ' %';
-      yAxisLabel = this.translate.get('GLOBAL.TARGET_MODALITY') + ' %';
-    }
-
-    this.chartOptions = {
-      interaction: {
-        intersect: true,
-        mode: 'nearest',
-        axis: 'x',
-      },
-      datasets: {
-        line: {
-          pointRadius: 0, // disable for all `'line'` datasets
-        },
-      },
-      elements: {
-        point: {
-          radius: 0, // default to disabled in all datasets
-        },
-      },
-      normalized: true,
-      animation: false,
-      plugins: {
-        tooltip: {
-          displayColors: true, // Show color square for curve names
-          callbacks: {
-            title: (items: TooltipItem<'line'>[]) =>
-              this.getTooltipTitle(items),
-            beforeBody: (items: TooltipItem<'line'>[]) =>
-              this.getTooltipBeforeBody(items),
-            label: (items: TooltipItem<'line'>) => this.getTooltipLabel(items),
-            afterLabel: (items: TooltipItem<'line'>) =>
-              this.getTooltipAfterLabel(items),
-          },
-        },
-      },
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: xAxisLabel,
-          },
-        },
-        y: {
-          title: {
-            display: true,
-            text: yAxisLabel,
-          },
-        },
-      },
+    // Initialize chart options using service
+    this.chartOptions = this.targetLiftGraphService.createChartOptions();
+    
+    // Override tooltip callbacks to use component context
+    this.chartOptions.plugins!.tooltip!.callbacks = {
+      title: (items) => this.targetLiftGraphService.getTooltipTitle(items, this.title),
+      beforeBody: (items) => this.targetLiftGraphService.getTooltipBeforeBody(items),
+      label: (items) => this.targetLiftGraphService.getTooltipLabel(items),
+      afterLabel: (items) => this.targetLiftGraphService.getTooltipAfterLabel(items),
     };
 
     this.buttonTitle = this.translate.get('GLOBAL.FILTER_CURVES');
@@ -151,170 +94,39 @@ export class TargetLiftGraphComponent
 
   /**
    * Fetches and updates the target lift data and graph data based on the selected target.
-   * If a target is selected, it retrieves the lift targets and graph data for that target.
-   * If no target is selected, it retrieves the default lift graph data.
+   * Uses the service to handle all the business logic.
    */
   private computeTargetLiftDatas() {
     const currentTarget = this.targetLift?.selected || undefined;
-    this.targetLift = this.evaluationDatasService.getLiftTargets(currentTarget);
-
-    if (this.targetLift) {
-      // Get previous selected target if compatible
-      const previousSelectedTarget = AppService.Ls.get(LS.TARGET_LIFT);
-      if (
-        previousSelectedTarget &&
-        this.targetLift.targets?.includes(previousSelectedTarget)
-      ) {
-        this.targetLift.selected = previousSelectedTarget;
-      }
-
-      this.titleWithoutDetails = this.translate.get(
-        'GLOBAL.CUMULATIVE_GAIN_CHART_OF',
-      );
-      this.title =
-        this.translate.get('GLOBAL.CUMULATIVE_GAIN_CHART_OF') +
-        ' ' +
-        this.targetLift.selected; // for copy graph datas
-      this.targetLiftGraph = this.evaluationDatasService.getLiftGraphDatas(
-        this.targetLift.selected,
-      );
-      // get all datas to copy
-      this.targetLiftAllGraph = this.evaluationDatasService.getLiftGraphDatas(
-        this.targetLift.selected,
-      );
-    } else {
-      // it is a regression
-      this.titleWithoutDetails = this.translate.get('GLOBAL.REC_CURVES');
-      this.title = this.translate.get('GLOBAL.REC_CURVES'); // for copy graph datas
-      this.targetLiftGraph = this.evaluationDatasService.getLiftGraphDatas();
-      // get all datas to copy
-      this.targetLiftAllGraph = this.evaluationDatasService.getLiftGraphDatas();
-    }
+    const computedData = this.targetLiftGraphService.computeTargetLiftData(currentTarget);
+    
+    // Update component properties with computed data
+    this.targetLift = computedData.targetLift;
+    this.targetLiftGraph = computedData.targetLiftGraph;
+    this.targetLiftAllGraph = computedData.targetLiftAllGraph;
+    this.colorSet = computedData.colorSet;
+    this.title = computedData.title;
+    this.titleWithoutDetails = computedData.titleWithoutDetails;
+    
+    // Initialize legend color set from the computed color set
+    this.legendColorSet = computedData.colorSet ? { ...computedData.colorSet } : undefined;
   }
 
   onSelectToggleButtonChanged(displayedValues: ChartToggleValuesI[]) {
     this.computeTargetLiftDatas();
 
-    this.colorSet = _.cloneDeep(
-      this.khiopsLibraryService.getGraphColorSet()[1],
-    );
-
-    // Remove hidden curves colors
-    let i = displayedValues.length;
-    while (i--) {
-      if (displayedValues[i]?.show === false) {
-        this.colorSet?.domain.splice(i, 1);
-      }
+    // Update color set based on displayed values using service
+    if (this.colorSet) {
+      this.colorSet = this.targetLiftGraphService.updateColorSetForDisplayedValues(
+        this.colorSet,
+        displayedValues
+      );
     }
   }
 
   changeTargetLift(target: string) {
     // this.trackerService.trackEvent('click', 'change_target_lift');
-    this.title =
-      this.translate.get('GLOBAL.CUMULATIVE_GAIN_CHART_OF') +
-      ' ' +
-      this.targetLift?.selected;
-    AppService.Ls.set(LS.TARGET_LIFT, target);
-    if (this.targetLift) {
-      this.targetLift.selected = target;
-    }
+    this.title = this.targetLiftGraphService.changeTarget(target, this.targetLift);
     this.computeTargetLiftDatas();
-  }
-
-  /**
-   * Get tooltip title for chart - displays this.title
-   * @param items - Tooltip items
-   * @returns Title string or undefined
-   */
-  private getTooltipTitle(items: TooltipItem<'line'>[]): string | undefined {
-    if (!items || items.length === 0) {
-      return undefined;
-    }
-    return this.title;
-  }
-
-  /**
-   * Get tooltip label for chart - displays curve name as currently shown
-   * @param items - Tooltip item
-   * @returns Label string or undefined
-   */
-  private getTooltipLabel(items: TooltipItem<'line'>): string | undefined {
-    if (items?.dataset?.label) {
-      return items.dataset.label;
-    }
-    return undefined;
-  }
-
-  /**
-   * Get tooltip after label for chart - displays Target modality for each curve
-   * @param items - Tooltip item
-   * @returns After label string or undefined
-   */
-  private getTooltipAfterLabel(items: TooltipItem<'line'>): string | undefined {
-    if (!items || items.dataIndex === undefined) {
-      return undefined;
-    }
-
-    const yValue = items.parsed?.y; // Target modality percentage from y-axis
-
-    if (yValue === undefined) {
-      return undefined;
-    }
-
-    if (this.evaluationDatasService.isRegressionAnalysis()) {
-      // For regression: y = Population %
-      return (
-        this.translate.get('GLOBAL.POPULATION') + ': ' + yValue.toFixed(2) + '%'
-      );
-    } else {
-      // For classification: y = Target Modality %
-      return (
-        this.translate.get('GLOBAL.TARGET_MODALITY') +
-        ': ' +
-        yValue.toFixed(2) +
-        '%'
-      );
-    }
-  }
-
-  /**
-   * Get tooltip before body for chart - displays Population info once (common to all curves)
-   * @param items - Tooltip items array
-   * @returns Before body string array or undefined
-   */
-  private getTooltipBeforeBody(
-    items: TooltipItem<'line'>[],
-  ): string[] | undefined {
-    if (
-      !items ||
-      items.length === 0 ||
-      !items[0] ||
-      items[0].dataIndex === undefined
-    ) {
-      return undefined;
-    }
-
-    // Use the first item to get the common x value (same for all curves at this point)
-    const firstItem = items[0];
-    const xValue = firstItem.label; // Population percentage from x-axis
-
-    const result: string[] = [];
-
-    if (this.evaluationDatasService.isRegressionAnalysis()) {
-      // For regression: x = Rank Error %
-      result.push(
-        this.translate.get('GLOBAL.RANK_ERROR') + ': ' + xValue + '%',
-      );
-    } else {
-      // For classification: x = Population %
-      result.push(
-        this.translate.get('GLOBAL.POPULATION') + ': ' + xValue + '%',
-      );
-    }
-
-    // Add empty line for margin bottom
-    result.push('');
-
-    return result;
   }
 }
---
+++ src/app/khiops-visualization/components/commons/target-lift-graph/target-lift-graph.service.ts
@@ -0,0 +1,256 @@
+/*
+ * Copyright (c) 2023-2025 Orange. All rights reserved.
+ * This software is distributed under the BSD 3-Clause-clear License, the text of which is available
+ * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
+ */
+
+import { Injectable } from '@angular/core';
+import { ChartOptions, TooltipItem } from 'chart.js';
+import { TranslateService } from '@ngstack/translate';
+import { EvaluationDatasService } from '@khiops-visualization/providers/evaluation-datas.service';
+import { KhiopsLibraryService } from '@khiops-library/providers/khiops-library.service';
+import { AppService } from '@khiops-visualization/providers/app.service';
+import { ChartDatasModel } from '@khiops-library/model/chart-datas.model';
+import { TargetLiftValuesI } from '@khiops-visualization/interfaces/target-lift-values';
+import { ChartColorsSetI } from '@khiops-library/interfaces/chart-colors-set';
+import { ChartToggleValuesI } from '@khiops-visualization/interfaces/chart-toggle-values';
+import { LS } from '@khiops-library/enum/ls';
+import _ from 'lodash';
+
+export interface TargetLiftGraphData {
+  targetLift?: TargetLiftValuesI;
+  targetLiftGraph?: ChartDatasModel;
+  targetLiftAllGraph?: ChartDatasModel;
+  colorSet?: ChartColorsSetI;
+  title: string;
+  titleWithoutDetails: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TargetLiftGraphService {
+
+  constructor(
+    private evaluationDatasService: EvaluationDatasService,
+    private translate: TranslateService,
+    private khiopsLibraryService: KhiopsLibraryService,
+  ) {}
+
+  /**
+   * Creates the chart options configuration for target lift graph
+   * @returns ChartOptions configuration
+   */
+  createChartOptions(): ChartOptions {
+    let xAxisLabel: string;
+    let yAxisLabel: string;
+    
+    if (this.evaluationDatasService.isRegressionAnalysis()) {
+      xAxisLabel = this.translate.get('GLOBAL.RANK_ERROR') + ' %';
+      yAxisLabel = this.translate.get('GLOBAL.POPULATION') + ' %';
+    } else {
+      xAxisLabel = this.translate.get('GLOBAL.POPULATION') + ' %';
+      yAxisLabel = this.translate.get('GLOBAL.TARGET_MODALITY') + ' %';
+    }
+
+    return {
+      interaction: {
+        intersect: true,
+        mode: 'nearest',
+        axis: 'x',
+      },
+      datasets: {
+        line: {
+          pointRadius: 0,
+        },
+      },
+      elements: {
+        point: {
+          radius: 0,
+        },
+      },
+      normalized: true,
+      animation: false,
+      plugins: {
+        tooltip: {
+          displayColors: true,
+          callbacks: {
+            title: (items: TooltipItem<'line'>[]) => this.getTooltipTitle(items),
+            beforeBody: (items: TooltipItem<'line'>[]) => this.getTooltipBeforeBody(items),
+            label: (items: TooltipItem<'line'>) => this.getTooltipLabel(items),
+            afterLabel: (items: TooltipItem<'line'>) => this.getTooltipAfterLabel(items),
+          },
+        },
+      },
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: xAxisLabel,
+          },
+        },
+        y: {
+          title: {
+            display: true,
+            text: yAxisLabel,
+          },
+        },
+      },
+    };
+  }
+
+  /**
+   * Computes and returns all target lift data including graph data and metadata
+   * @param currentTarget - Current selected target (optional)
+   * @returns TargetLiftGraphData object containing all computed data
+   */
+  computeTargetLiftData(currentTarget?: string): TargetLiftGraphData {
+    const targetLift = this.evaluationDatasService.getLiftTargets(currentTarget);
+    
+    const colorSet = _.cloneDeep(this.khiopsLibraryService.getGraphColorSet()[1]);
+    let title: string;
+    let titleWithoutDetails: string;
+    let targetLiftGraph: ChartDatasModel | undefined;
+    let targetLiftAllGraph: ChartDatasModel | undefined;
+
+    if (targetLift) {
+      // Get previous selected target if compatible
+      const previousSelectedTarget = AppService.Ls.get(LS.TARGET_LIFT);
+      if (previousSelectedTarget && targetLift.targets?.includes(previousSelectedTarget)) {
+        targetLift.selected = previousSelectedTarget;
+      }
+
+      titleWithoutDetails = this.translate.get('GLOBAL.CUMULATIVE_GAIN_CHART_OF');
+      title = this.translate.get('GLOBAL.CUMULATIVE_GAIN_CHART_OF') + ' ' + targetLift.selected;
+      
+      targetLiftGraph = this.evaluationDatasService.getLiftGraphDatas(targetLift.selected);
+      targetLiftAllGraph = this.evaluationDatasService.getLiftGraphDatas(targetLift.selected);
+    } else {
+      // Regression case
+      titleWithoutDetails = this.translate.get('GLOBAL.REC_CURVES');
+      title = this.translate.get('GLOBAL.REC_CURVES');
+      
+      targetLiftGraph = this.evaluationDatasService.getLiftGraphDatas();
+      targetLiftAllGraph = this.evaluationDatasService.getLiftGraphDatas();
+    }
+
+    return {
+      targetLift,
+      targetLiftGraph,
+      targetLiftAllGraph,
+      colorSet,
+      title,
+      titleWithoutDetails
+    };
+  }
+
+  /**
+   * Updates color set based on displayed values by removing colors for hidden curves
+   * @param colorSet - Original color set
+   * @param displayedValues - Array of chart toggle values
+   * @returns Updated color set
+   */
+  updateColorSetForDisplayedValues(
+    colorSet: ChartColorsSetI, 
+    displayedValues: ChartToggleValuesI[]
+  ): ChartColorsSetI {
+    const updatedColorSet = _.cloneDeep(colorSet);
+    
+    let i = displayedValues.length;
+    while (i--) {
+      if (displayedValues[i]?.show === false) {
+        updatedColorSet.domain.splice(i, 1);
+      }
+    }
+    
+    return updatedColorSet;
+  }
+
+  /**
+   * Handles target change and saves to local storage
+   * @param target - New target value
+   * @param targetLift - Current target lift data
+   * @returns Updated title
+   */
+  changeTarget(target: string, targetLift?: TargetLiftValuesI): string {
+    AppService.Ls.set(LS.TARGET_LIFT, target);
+    
+    if (targetLift) {
+      targetLift.selected = target;
+    }
+    
+    return this.translate.get('GLOBAL.CUMULATIVE_GAIN_CHART_OF') + ' ' + target;
+  }
+
+  /**
+   * Get tooltip title for chart
+   * @param items - Tooltip items
+   * @param title - Chart title
+   * @returns Title string or undefined
+   */
+  getTooltipTitle(items: TooltipItem<'line'>[], title?: string): string | undefined {
+    if (!items || items.length === 0) {
+      return undefined;
+    }
+    return title;
+  }
+
+  /**
+   * Get tooltip label for chart - displays curve name
+   * @param items - Tooltip item
+   * @returns Label string or undefined
+   */
+  getTooltipLabel(items: TooltipItem<'line'>): string | undefined {
+    if (items?.dataset?.label) {
+      return items.dataset.label;
+    }
+    return undefined;
+  }
+
+  /**
+   * Get tooltip after label for chart - displays Target modality for each curve
+   * @param items - Tooltip item
+   * @returns After label string or undefined
+   */
+  getTooltipAfterLabel(items: TooltipItem<'line'>): string | undefined {
+    if (!items || items.dataIndex === undefined) {
+      return undefined;
+    }
+
+    const yValue = items.parsed?.y;
+
+    if (yValue === undefined) {
+      return undefined;
+    }
+
+    if (this.evaluationDatasService.isRegressionAnalysis()) {
+      return this.translate.get('GLOBAL.POPULATION') + ': ' + yValue.toFixed(2) + '%';
+    } else {
+      return this.translate.get('GLOBAL.TARGET_MODALITY') + ': ' + yValue.toFixed(2) + '%';
+    }
+  }
+
+  /**
+   * Get tooltip before body for chart - displays Population info once (common to all curves)
+   * @param items - Tooltip items array
+   * @returns Before body string array or undefined
+   */
+  getTooltipBeforeBody(items: TooltipItem<'line'>[]): string[] | undefined {
+    if (!items || items.length === 0 || !items[0] || items[0].dataIndex === undefined) {
+      return undefined;
+    }
+
+    const firstItem = items[0];
+    const xValue = firstItem.label;
+    const result: string[] = [];
+
+    if (this.evaluationDatasService.isRegressionAnalysis()) {
+      result.push(this.translate.get('GLOBAL.RANK_ERROR') + ': ' + xValue + '%');
+    } else {
+      result.push(this.translate.get('GLOBAL.POPULATION') + ': ' + xValue + '%');
+    }
+
+    result.push(''); // Add empty line for margin
+    return result;
+  }
+}
