4d7836306be60e6dd00c1a59b1ae686ff83374bb
Stéphane Bouget <stephane.bouget@orange.com>
2025-10-27 15:27:40 +0100

    Fix ts config warnings

--- src/tests/visualization/copy-datas.service.spec.ts
+++ src/tests/visualization/copy-datas.service.spec.ts
@@ -3,7 +3,6 @@
  * This software is distributed under the BSD 3-Clause-clear License, the text of which is available
  * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
  */
-// @ts-nocheck
 
 import { TestBed } from '@angular/core/testing';
 
