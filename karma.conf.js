a76cad5c8187f1c586ee7c248d24df4dca4c6e76
Stéphane Bouget <stephane.bouget@orange.com>
2025-11-12 16:39:36 +0100

    Try to launch karma with ChromeHeadlessNoSandbox

--- karma.conf.js
+++ karma.conf.js
@@ -6,41 +6,46 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-	  require('karma-jasmine'),
-	  require('karma-chrome-launcher'),
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      jasmine: {},
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/visualization-component'),
+      dir: require('path').join(
+        __dirname,
+        './coverage/visualization-component',
+      ),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    // logLevel: config.LOG_INFO,
     logLevel: config.LOG_DEBUG,
-    // autoWatch: true,
-    browsers: ['ChromeHeadless'],
-	// browsers: ['Chrome'],
-	singleRun: true,
-    // restartOnFileChange: true
+
+    // ✅ Custom launcher to run Chrome without sandbox (required with VS Code --no-sandbox)
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
+      },
+    },
+
+    // ❌ Don't use ChromeHeadless (blocked by enterprise sandbox policy)
+    // browsers: ['ChromeHeadless'],
+
+    // ✅ Use the no-sandbox launcher instead
+    browsers: ['ChromeHeadlessNoSandbox'],
+
+    singleRun: true,
   });
 };
