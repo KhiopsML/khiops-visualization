ca80ea6c8f0fc71692317f23ba8251c36a6b4db3
Stéphane Bouget <stephane.bouget@orange.com>
2025-11-13 09:19:16 +0100

    Remove unused comment

--- src/app/khiops-visualization/providers/preparation-datas.service.ts
+++ src/app/khiops-visualization/providers/preparation-datas.service.ts
@@ -296,8 +296,6 @@ export class PreparationDatasService {
             );
             if (index !== 0) {
               // replace [ by ] for all indexes excepting 0
-              // currentPartition = currentPartition.replace('[', ']');
-              // Code scanning alerts #3
               currentPartition = currentPartition.replace(/\[/g, ']');
             }
             datas[0]['interval'] = currentPartition;
