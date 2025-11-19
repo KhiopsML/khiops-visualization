757ddd48a57e9b2b242cb93c86223cfb9d9986d1
Stéphane Bouget <stephane.bouget@orange.com>
2025-11-19 09:40:09 +0100

    Remove obsolete PowerShell build scripts for Windows environments

--- scripts/buildHyperTree.ps1
+++
@@ -1,6 +0,0 @@
-# Build HyperTree script for Windows PowerShell
-# Alternative to buildHyperTree.sh for Windows environments
-
-Set-Location "libs/khiops-hypertree"
-yarn build
-Set-Location "../.."
