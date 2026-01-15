e05c47c749b48e306f49cf5e1ac36bea12df49ff
BOUGET Stephane INNOV/IT-S <stephane.bouget@orange.com>
2026-01-15 16:52:38 +0100

    fix: add missing screenshot tests for various preparation tabs in copy images test plan

--- cypress/e2e/visualization/copy-image.spec.cy.ts
+++ cypress/e2e/visualization/copy-image.spec.cy.ts
@@ -23,6 +23,7 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
           cy.spy(win, 'fetch').as('fetchSpy');
         });
 
+        // Preparation Tab Screenshots
         cy.get('.mat-mdc-tab:contains("Preparation")').first().click();
         cy.wait(500);
         cy.testComponentScreenshot('#preparation-informations-block-summary');
@@ -30,6 +31,13 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot(
           '#preparation-informations-block-informations',
         );
+        cy.get('.level-distribution-btn').first().click({ force: true });
+        cy.wait(250);
+        cy.testComponentScreenshot('#level-distribution-graph-comp');
+        cy.get('#level-distribution-graph-close-btn')
+          .first()
+          .click({ force: true });
+        cy.wait(250);
         cy.testComponentScreenshot('#preparation-variables-list');
         cy.testComponentScreenshot('#distribution-graph0');
         cy.testComponentScreenshot('#target-distribution-graph0');
@@ -37,6 +45,7 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot('#preparation-description-block-derivation');
         cy.testComponentScreenshot('#preparation-current-interval');
 
+        // Text preparation Tab Screenshots
         cy.get('.mat-mdc-tab:contains("Text preparation")').first().click();
         cy.wait(500);
         cy.testComponentScreenshot('#preparation-informations-block-summary');
@@ -44,6 +53,13 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot(
           '#preparation-informations-block-informations',
         );
+        cy.get('.level-distribution-btn').first().click({ force: true });
+        cy.wait(250);
+        cy.testComponentScreenshot('#level-distribution-graph-comp');
+        cy.get('#level-distribution-graph-close-btn')
+          .first()
+          .click({ force: true });
+        cy.wait(250);
         cy.testComponentScreenshot('#preparation-variables-list');
         cy.testComponentScreenshot('#distribution-graph0');
         cy.testComponentScreenshot('#target-distribution-graph0');
@@ -51,6 +67,7 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot('#preparation-description-block-derivation');
         cy.testComponentScreenshot('#preparation-current-interval');
 
+        // Tree preparation Tab Screenshots
         cy.get('.mat-mdc-tab:contains("Tree preparation")').first().click();
         cy.wait(500);
         cy.testComponentScreenshot(
@@ -60,6 +77,13 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot(
           '#tree-preparation-informations-block-informations',
         );
+        cy.get('.level-distribution-btn').first().click({ force: true });
+        cy.wait(250);
+        cy.testComponentScreenshot('#level-distribution-graph-comp');
+        cy.get('#level-distribution-graph-close-btn')
+          .first()
+          .click({ force: true });
+        cy.wait(250);
         cy.testComponentScreenshot('#tree-preparation-variables-list');
         cy.testComponentScreenshot('#distribution-graph0');
         cy.testComponentScreenshot('#target-distribution-graph0');
@@ -68,11 +92,19 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot('#tree-details-comp');
         cy.testComponentScreenshot('#tree-preparation-hyper');
 
+        // Preparation 2D Tab Screenshots
         cy.get('.mat-mdc-tab:contains("Preparation 2D")').first().click();
         cy.wait(500);
         cy.testComponentScreenshot(
           '#preparation-2d-informations-block-summary',
         );
+        cy.get('.level-distribution-btn').first().click({ force: true });
+        cy.wait(250);
+        cy.testComponentScreenshot('#level-distribution-graph-comp');
+        cy.get('#level-distribution-graph-close-btn')
+          .first()
+          .click({ force: true });
+        cy.wait(250);
         cy.testComponentScreenshot('#preparation-2d-target-variable-stats');
         cy.testComponentScreenshot('#preparation-2d-variables-list');
         cy.testComponentScreenshot('#cooccurrence-matrix-comp');
@@ -80,6 +112,7 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot('#preparation-2d-current-cell-x');
         cy.testComponentScreenshot('#preparation-2d-current-cell-y');
 
+        // Modeling Tab Screenshots
         cy.get('.mat-mdc-tab:contains("Modeling")').first().click();
         cy.wait(500);
         cy.testComponentScreenshot('#modeling-informations-block-summary');
@@ -90,6 +123,7 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot('#target-distribution-graph0');
         cy.testComponentScreenshot('#preparation-current-interval');
 
+        // Evaluation Tab Screenshots
         cy.get('.mat-mdc-tab:contains("Evaluation")').first().click();
         cy.wait(500);
         cy.testComponentScreenshot('#evaluation-types-summary');
@@ -97,6 +131,7 @@ describe('Copy images Test Plan for Khiops Visualization', () => {
         cy.testComponentScreenshot('#evaluation-lift-curves');
         cy.testComponentScreenshot('#evaluation-confusion-matrix');
 
+        // Project Tab Screenshots
         cy.get('.mat-mdc-tab:contains("Project")').first().click();
         cy.wait(500);
         cy.testComponentScreenshot('#project-logs');
