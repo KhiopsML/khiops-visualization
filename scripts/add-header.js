98122c3c1c884d5472831a725128f4e29e4478da
Stéphane Bouget <stephane.bouget@orange.com>
2026-03-02 16:38:52 +0100

    feat: implement dynamic headers for multiple file formats in add-header.js

--- scripts/add-header.js
+++ scripts/add-header.js
@@ -4,23 +4,63 @@ const path = require('path');
 // Chemin du projet Angular
 const projectDir = './src';
 
-// Header à ajouter
-const headerComment = `/*
+// Copyright notice
+const copyrightNotice = 'Copyright (c) 2023-2025 Orange. All rights reserved.';
+
+// Headers par format de fichier
+const headers = {
+  ts: `/*
  * Copyright (c) 2023-2025 Orange. All rights reserved.
  * This software is distributed under the BSD 3-Clause-clear License, the text of which is available
  * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
  */
 
-`;
+`,
+  js: `/*
+ * Copyright (c) 2023-2025 Orange. All rights reserved.
+ * This software is distributed under the BSD 3-Clause-clear License, the text of which is available
+ * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
+ */
+
+`,
+  scss: `/*
+ * Copyright (c) 2023-2025 Orange. All rights reserved.
+ * This software is distributed under the BSD 3-Clause-clear License, the text of which is available
+ * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
+ */
+
+`,
+  html: `<!--
+ * Copyright (c) 2023-2025 Orange. All rights reserved.
+ * This software is distributed under the BSD 3-Clause-clear License, the text of which is available
+ * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
+ -->
+
+`
+};
+
+function headerExists(fileContent) {
+  return fileContent.includes(copyrightNotice);
+}
+
+function getFileExtension(filePath) {
+  const ext = path.extname(filePath).slice(1);
+  return ext;
+}
 
 function addHeaderToFile(filePath) {
   const fileContent = fs.readFileSync(filePath, 'utf8');
-  if (!fileContent.startsWith('/**')) {
-    const newContent = headerComment + fileContent;
-    fs.writeFileSync(filePath, newContent, 'utf8');
-    console.log(`Header ajouté à : ${filePath}`);
-  } else {
+  const ext = getFileExtension(filePath);
+
+  if (headerExists(fileContent)) {
     console.log(`Header déjà présent dans : ${filePath}`);
+  } else {
+    const header = headers[ext];
+    if (header) {
+      const newContent = header + fileContent;
+      fs.writeFileSync(filePath, newContent, 'utf8');
+      console.log(`Header ajouté à : ${filePath}`);
+    }
   }
 }
 
@@ -29,8 +69,11 @@ function traverseDirectory(directory) {
     const fullPath = path.join(directory, file);
     if (fs.statSync(fullPath).isDirectory()) {
       traverseDirectory(fullPath);
-    } else if (fullPath.endsWith('.js') || fullPath.endsWith('.ts')) {
-      addHeaderToFile(fullPath);
+    } else {
+      const ext = getFileExtension(fullPath);
+      if (headers[ext]) {
+        addHeaderToFile(fullPath);
+      }
     }
   });
 }
