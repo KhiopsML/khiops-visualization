f73e7368ce8452d222cdf852969826e14ce2d0e2
Stéphane Bouget <stephane.bouget@orange.com>
2026-04-30 15:36:58 +0200

    fix: Set default LS_ID for covisualization and visualization components

--- src/app/khiops-visualization/khiops-visualization.component.ts
+++ src/app/khiops-visualization/khiops-visualization.component.ts
@@ -147,7 +147,11 @@ export class AppComponent
       cb('reject');
     };
     this.element.nativeElement.setConfig = (config: ConfigModel) => {
-      AppService.Ls.setLsId(config.lsId);
+      AppService.Ls.setLsId(
+        config.lsId ||
+          AppConfig.visualizationCommon.GLOBAL.LS_ID ||
+          'KHIOPS_VISUALIZATION_',
+      );
 
       this.configService.setConfig(config);
 
