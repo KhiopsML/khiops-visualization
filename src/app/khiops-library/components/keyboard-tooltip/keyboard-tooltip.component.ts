17d1ce97262f7179a2697014936c5f0a255a1cbd
BOUGET Stephane INNOV/IT-S <stephane.bouget@orange.com>
2026-06-01 14:55:06 +0200

    fix: update translation service import and streamline tooltip key replacement logic

--- src/app/khiops-library/components/keyboard-tooltip/keyboard-tooltip.component.ts
+++ src/app/khiops-library/components/keyboard-tooltip/keyboard-tooltip.component.ts
@@ -4,7 +4,7 @@
  * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
  */
 import { Component, Input, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService } from '@ngstack/translate';
 
 @Component({
   selector: 'kl-keyboard-tooltip',
@@ -15,28 +15,27 @@ import { TranslateService } from '@ngx-translate/core';
 export class KeyboardTooltipComponent implements OnInit {
   @Input() key: string = '';
   @Input() tooltip: string = '';
-  
+
   displayKey: string = '';
   tooltipWithCorrectKey: string = '';
-  
-  private isMac =
-    typeof navigator !== 'undefined' &&
-    /mac/i.test(navigator.userAgent);
 
-  constructor(private translateService: TranslateService) {}
+  private isMac =
+    typeof navigator !== 'undefined' && /mac/i.test(navigator.userAgent);
+
+  constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {
     // Detect platform and use CMD for Mac, CTRL for others
     if (this.key) {
       this.displayKey = this.key === 'CTRL' && this.isMac ? 'CMD' : this.key;
     }
-    
+
     // Replace the key in the tooltip to show the correct key for the platform
     if (this.tooltip) {
-      this.translateService.get(this.tooltip).subscribe((translatedText: string) => {
-        const correctKey = this.isMac ? 'Cmd' : 'Ctrl';
-        this.tooltipWithCorrectKey = translatedText.replace('Ctrl', correctKey);
-      });
+      const correctKey = this.isMac ? 'Cmd' : 'Ctrl';
+      this.tooltipWithCorrectKey = this.translate
+        .get(this.tooltip)
+        .replace('Ctrl', correctKey);
     }
   }
 }
