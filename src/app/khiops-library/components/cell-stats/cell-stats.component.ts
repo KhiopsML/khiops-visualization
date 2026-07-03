7bca3074619fe3e291a50d5ba6722fb1aa504bd1
Stéphane Bouget <stephane.bouget@orange.com>
2026-07-03 10:01:27 +0200

    refactor: update cellStats computation to use computed properties and simplify logic

--- src/app/khiops-library/components/cell-stats/cell-stats.component.ts
+++ src/app/khiops-library/components/cell-stats/cell-stats.component.ts
@@ -4,66 +4,56 @@
  * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
  */
 
-import { Component, SimpleChanges, OnChanges, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 import { UtilsService } from '../../providers/utils.service';
 import { CellModel } from '@khiops-library/model/cell.model';
 import { MATRIX_MODES } from '@khiops-library/enum/matrix-modes';
 import { CellStatsI } from '@khiops-library/interfaces/cell-stats.interface';
 
 @Component({
-    selector: 'kl-cell-stats',
-    templateUrl: './cell-stats.component.html',
-    styleUrls: ['./cell-stats.component.scss'],
-    standalone: false
+  selector: 'kl-cell-stats',
+  templateUrl: './cell-stats.component.html',
+  styleUrls: ['./cell-stats.component.scss'],
+  standalone: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CellStatsComponent implements OnChanges {
-  @Input() private selectedCells: CellModel[] = [];
+export class CellStatsComponent {
+  readonly selectedCells = input<CellModel[]>([]);
 
-  public cellStats: CellStatsI | undefined;
+  readonly cellStats = computed<CellStatsI | undefined>(() =>
+    this.computeCellStats(this.selectedCells()),
+  );
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes?.selectedCells?.currentValue) {
-      if (changes?.selectedCells?.currentValue.length > 1) {
-        // Multiple selection
-        this.cellStats = {
-          F: UtilsService.arraySum(
-            this.selectedCells.map((e) => e.displayedFreqValue),
-          ),
-          EF: UtilsService.arraySum(
-            this.selectedCells.map((e) => e.displayedValue?.ef),
-          ),
-          I:
-            this.selectedCells[0]?.displayedValue?.type ===
-            MATRIX_MODES.MUTUAL_INFO
-              ? UtilsService.arraySum(
-                  this.selectedCells.map((e) => e.displayedValue.value),
-                )
-              : undefined,
-          Total:
-            this.selectedCells[0]?.displayedValue?.type ===
-            MATRIX_MODES.MUTUAL_INFO
-              ? UtilsService.arraySum(
-                  this.selectedCells.map((e) => e.displayedValue.extra),
-                )
-              : undefined,
-        };
-      } else if (changes?.selectedCells?.currentValue.length === 1) {
-        // Simple selection
-        this.cellStats = {
-          F: this.selectedCells[0]?.displayedFreqValue,
-          EF: this.selectedCells[0]?.displayedValue?.ef,
-          I:
-            this.selectedCells[0]?.displayedValue?.type ===
-            MATRIX_MODES.MUTUAL_INFO
-              ? this.selectedCells[0]?.displayedValue?.value
-              : undefined,
-          Total:
-            this.selectedCells[0]?.displayedValue?.type ===
-            MATRIX_MODES.MUTUAL_INFO
-              ? this.selectedCells[0]?.displayedValue?.extra
-              : undefined,
-        };
-      }
+  private computeCellStats(selectedCells: CellModel[]): CellStatsI | undefined {
+    if (!selectedCells?.length) {
+      return undefined;
     }
+
+    const isMutualInfoMode =
+      selectedCells[0]?.displayedValue?.type === MATRIX_MODES.MUTUAL_INFO;
+
+    return {
+      F: UtilsService.arraySum(
+        selectedCells.map((cell) => cell.displayedFreqValue),
+      ),
+      EF: UtilsService.arraySum(
+        selectedCells.map((cell) => cell.displayedValue?.ef),
+      ),
+      I: isMutualInfoMode
+        ? UtilsService.arraySum(
+            selectedCells.map((cell) => cell.displayedValue?.value),
+          )
+        : undefined,
+      Total: isMutualInfoMode
+        ? UtilsService.arraySum(
+            selectedCells.map((cell) => cell.displayedValue?.extra),
+          )
+        : undefined,
+    };
   }
 }
