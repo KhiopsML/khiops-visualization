fc36e94e64fe7a331e5b13480cebc4cc22e4cb85
Stéphane Bouget <stephane.bouget@orange.com>
2026-07-29 14:43:01 +0200

    feat: refactor ProjectLogsComponent to use Angular signals for state management and update template bindings

--- src/app/khiops-visualization/components/commons/project-logs/project-logs.component.ts
+++ src/app/khiops-visualization/components/commons/project-logs/project-logs.component.ts
@@ -4,64 +4,57 @@
  * at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
  */
 
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { GridColumnsI } from '@khiops-library/interfaces/grid-columns.interface';
 import { ProjectLogModel } from '@khiops-library/model/project-log.model';
 import { ProjectDatasService } from '@khiops-visualization/providers/project-datas.service';
 import { TranslateService } from '@ngstack/translate';
-import { Subscription } from 'rxjs';
 import { FileLoaderService } from '@khiops-library/providers/file-loader.service';
 import { GravityCellComponent } from './gravity-cell/gravity-cell.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { KhiopsLibraryModule } from '@khiops-library/khiops-library.module';
 
 @Component({
   selector: 'app-project-logs',
   templateUrl: './project-logs.component.html',
   styleUrls: ['./project-logs.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Eager,
-  standalone: false,
+  imports: [FlexLayoutModule, KhiopsLibraryModule],
 })
 export class ProjectLogsComponent {
-  public projectLogsDatas?: ProjectLogModel[];
-  public logsTitle: string;
-  public logsDisplayedColumns: GridColumnsI[] = [];
-  private fileLoadedSub?: Subscription;
+  private readonly fileLoaderService = inject(FileLoaderService);
+  private readonly projectDatasService = inject(ProjectDatasService);
+  private readonly translate = inject(TranslateService);
 
-  constructor(
-    private fileLoaderService: FileLoaderService,
-    private projectDatasService: ProjectDatasService,
-    private translate: TranslateService,
-  ) {
-    this.logsTitle = this.translate.get('GLOBAL.LOGS');
+  public readonly projectLogsDatas = signal<ProjectLogModel[] | undefined>(
+    undefined,
+  );
+  public readonly logsTitle = signal(this.translate.get('GLOBAL.LOGS'));
+  public readonly logsDisplayedColumns = signal<GridColumnsI[]>([
+    {
+      headerName: this.translate.get('GLOBAL.TASK'),
+      field: 'task',
+    },
+    {
+      headerName: this.translate.get('GLOBAL.GRAVITY'),
+      field: 'gravity',
+      cellRenderer: GravityCellComponent,
+    },
+    {
+      headerName: this.translate.get('GLOBAL.MESSAGE'),
+      field: 'message',
+    },
+  ]);
 
-    this.logsDisplayedColumns = [
-      {
-        headerName: this.translate.get('GLOBAL.TASK'),
-        field: 'task',
-      },
-      {
-        headerName: this.translate.get('GLOBAL.GRAVITY'),
-        field: 'gravity',
-        cellRenderer: GravityCellComponent,
-      },
-      {
-        headerName: this.translate.get('GLOBAL.MESSAGE'),
-        field: 'message',
-      },
-    ];
-  }
-
-  ngAfterViewInit() {
-    this.fileLoadedSub = this.fileLoaderService.fileLoaded$.subscribe(
-      (datas) => {
+  constructor() {
+    this.fileLoaderService.fileLoaded$
+      .pipe(takeUntilDestroyed())
+      .subscribe((datas) => {
         if (datas) {
-          this.projectLogsDatas =
-            this.projectDatasService.getProjectLogsDatas();
+          this.projectLogsDatas.set(
+            this.projectDatasService.getProjectLogsDatas(),
+          );
         }
-      },
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.fileLoadedSub?.unsubscribe();
+      });
   }
 }
