f5358350674d7925ac09b22e251c99fbc686d9ef
Stéphane Bouget <stephane.bouget@orange.com>
2026-09-25 16:23:35 +0200

    feat: update font-family to use var(--font-code) for consistency across components

--- src/app/khiops-library/config/mock-files.ts
+++ src/app/khiops-library/config/mock-files.ts
@@ -10,7 +10,8 @@
  */
 export function getVisualizationMockFile(): string {
   // let urlKV = 'ALLREPORTS_Std_Iris_AnalysisResults.khj';
-  let urlKV = 'demo-visualization.json';
+  // let urlKV = 'demo-visualization.json';
+  let urlKV = '2d-cells-AllReports.json ';
   // let urlKV = '312-AnalysisResults.khj';
   // let urlKV = 'AnalysisResults-IrisRegressionWithTree.khj';
   // let urlKV = 'new-hyper-tree.json';
